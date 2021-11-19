09f2b3089a97fef57c8eb23d5ee34d42b6e38fbd
Tuan anh <88386260+Linh2908@users.noreply.github.com>
2021-11-19 10:35:28 +0700

    update

---
+++ array/main.js
@@ -0,0 +1,15 @@
+let x = 0;
+let arr = [];
+document.querySelector('#btnAdd').onclick = function(){
+   arr[x] = document.querySelector('#textValue').value;
+    x++;
+    document.querySelector("#textValue ").value = "";
+}
+document.querySelector('#btnDisplay').onclick = function(){
+    let e = '<hr>';
+    let arrLength = arr.length;
+    for(let i = 0;i<arrLength;i++){
+        e += `Element ${i} = ${arr[i]} <br>`;
+    }
+    document.querySelector('#result').innerHTML = e;
+}
